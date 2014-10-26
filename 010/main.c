f2e01f1c60881c0c32028031ae303ca31729a83c
Sergey Fukanchik <fukanchik@gmail.com>
2014-10-26 23:11:37 +0400

    Adding task 010

---
+++ 010/main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <math.h>
+
+#include <euler/config.h>
+
+#define SIZE 2000000
+
+static ULL primes[SIZE];
+static int current;
+
+static int
+isprime(int num)
+{
+	int half = sqrt(num) + 1;
+	int i;
+
+	for(i = 0; i < current && primes[i] < half; ++i)
+		if((num % primes[i]) == 0)
+			return 0;
+
+	primes[current++] = num;
+
+	return 1;
+}
+
+int
+main(int argc, char* argv[])
+{
+	ULL sum;
+	int i;
+
+	primes[0]=2;
+	current=1;
+
+	for( sum = 2, i = 3; i < SIZE; i += 2)
+		sum += i * isprime(i);
+
+	printf(ULLFMT"\n", sum);
+
+	return 0;
+}
