7e30fb5edf9eb4e007cce66c8b879d0a20a1caba
Sergey Fukanchik <fukanchik@gmail.com>
2014-10-27 00:37:52 +0400

    Adding task 100

---
+++ 100/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+#include <euler/config.h>
+
+#define LD long double
+#define MAX 1000000000000ll
+
+static ULL
+is_50(ULL blue, ULL red)
+{
+	return 2000.0 * 
+			(LD)blue / ((LD)blue+(LD)red)
+			*
+			((LD)blue-1.0)/((LD)blue+(LD)red-1.0);
+}
+
+int
+main(int argc, char *argv[])
+{
+	ULL i, j;
+
+	printf("%llu\n", is_50(11, 7));
+	printf("%llu\n", is_50(15, 6));
+	printf("%llu\n", is_50(85, 35));
+
+	for(i=1ll; i < MAX; ++i) {
+		j = MAX - i;
+		ULL v = is_50(i, j);
+		if(v != 1000ll) continue;
+		printf(ULLFMT " XX " ULLFMT " = " ULLFMT "\n", i, j, v);
+		if(v == 1000ll) break;
+	}
+
+	return 0;
+}
