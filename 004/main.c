8ea358269286032255caa7cafb0edac90087b93a
Sergey Fukanchik <fukanchik@gmail.com>
2014-10-26 23:01:42 +0400

    Adding task 004

---
+++ 004/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+#include <euler/palindrome.h>
+
+int
+main(int argc, char* argv[])
+{
+	int i, j;
+	int maxpal;
+
+	for(maxpal = 0, i = 100; i <= 999; ++i)
+	{
+		for(j=999; j>=100;--j) {
+			int num=i*j;
+			if(ispal10(num))
+			{
+				if(num>maxpal) {
+					maxpal=num;
+				}
+			}
+		}
+	}
+
+	if(maxpal != 0)
+		printf("%d\n", maxpal);
+
+	return 0;
+}
