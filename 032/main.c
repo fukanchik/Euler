fb7be85302ce4bb9a42b4ae16078abcef960848e
Sergey Fukanchik <fukanchik@gmail.com>
2014-10-27 00:04:16 +0400

    Adding task 032

---
+++ 032/main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <euler/pandigital.h>
+
+int
+find(int *a, int l, int v)
+{
+	int i;
+	for(i=0;i<l;++i)if(a[i]==v)return 1;
+	return 0;
+}
+int
+main(int argc, char *argv[])
+{
+	int products[100];
+	int nprods=0;
+	int sum = 0;
+	int a;
+	int b;
+
+	for(a=1;a<5000;a++)
+	{
+		for(b=1;b<5000;b++)
+		{
+			int prod = a * b;
+			if(ispandigital(a, b, prod))
+			{
+				if(!find(products, nprods, prod))
+				{
+					products[nprods++]=prod;
+					sum += prod;
+				}
+			}
+		}
+	}
+	printf("%d\n", sum);
+	return 0;
+}
+
