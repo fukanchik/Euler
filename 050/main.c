742400e36f491fac8be464f354841a6ef078e2f2
Sergey Fukanchik <fukanchik@gmail.com>
2014-10-27 00:23:26 +0400

    Adding task 050

---
+++ 050/main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+#include <euler/primes.h>
+
+static int
+isprime(struct prime_list_t *primes, int num)
+{
+	int i;
+
+	for(i=primes->count - 1;primes->primes[i] >= num && i>=0;--i)
+	{
+		if(primes->primes[i]==num) return 1;
+	}
+	return 0;
+}
+
+int
+main(int argc, char *argv[])
+{
+	struct prime_list_t primes;
+	int sum, i;
+
+	primes_init_fill(&primes, 1000000);
+
+	for(sum = 0, i = 0;;i++)
+	{
+		if(sum + primes.primes[i] > 1000000) break;
+		sum += primes.primes[i];
+	}
+	for(i = 0;i<1000000;)
+	{
+		if(isprime(&primes, sum))
+			break;
+		sum -= primes.primes[i];
+	}
+	printf("%d\n", sum);
+
+	return 0;
+}
+
