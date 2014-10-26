474456d9168b2b80d1c4814ac40d35ba386f9e75
Sergey Fukanchik <fukanchik@gmail.com>
2014-10-27 00:00:10 +0400

    Adding task 026

---
+++ 026/main.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+
+int
+nmultiplier(int num)
+{
+	if(num<0)return 0;
+	if(num<10)return 1;
+	if(num<100)return 2;
+	if(num<1000)return 3;
+	return 0;
+}
+
+int
+pow10(int n)
+{
+	if(n==0)return 1;
+	if(n==1)return 10;
+	if(n==2)return 100;
+	if(n==3) return 1000;
+	return 0;
+}
+
+int find(int* a, int l, int n){
+	int i;
+
+	for(i=0;i<l;++i)
+	{
+		if(a[i]==n)return 1;
+	}
+	return 0;
+}
+
+int
+main(int argc, char* argv[])
+{
+	int div;
+	int max=0;
+	int val;
+
+	for(div=1;div<1000;++div) {
+		int i;
+		int digits[1000];
+		int ndigits;
+		int num = 1;
+		int nn = nmultiplier(div);
+		memset(digits, 0, sizeof(digits));
+		ndigits=0;
+
+		for(i=0;i<1000;++i)
+		{
+			num *= pow10(nn);
+		
+			num %= div;
+
+			if(!find(digits, ndigits, num))
+				digits[ndigits++]=num;
+		}
+		if(ndigits>max)
+		{
+			max = ndigits;
+			val=div;
+		}
+	}
+	printf("%d\n", val);
+	return 0;
+}
+
