ca6a565fbca9ab4a6b69135b53605414a9ef4e64
Sergey Fukanchik <s.fukanchik@postgrespro.ru>
2026-02-16 11:44:32 +0300

    Faster solution for problem #14

--- 014/main.c
+++ 014/main.c
@@ -1,41 +1,59 @@
+#include <euler/config.h>
 #include <stdio.h>
 
-#include <euler/config.h>
+#define COUNT 1000000
+
+static int lens[COUNT];
 
 static u8
 next(u8 c)
 {
-	if(c % 2 == 0)
-		return c / 2;
+	if (c % 2 == 0) return c / 2;
 
 	return 3 * c + 1;
 }
 
-static int len(u8 n)
+static int
+len(u8 n)
 {
+	if (n < COUNT && lens[n] != 0) return lens[n];
+
+	const u8  orig  = n;
 	int count = 0;
-	while(n > 1) {
+	while (n > 1)
+	{
 		n = next(n);
+		if (n < COUNT && lens[n] != 0)
+		{
+			lens[orig] = lens[n] + count;
+			return lens[n] + count;
+		}
 		count++;
 	}
-	return count+1;
+
+	if (orig < COUNT) lens[orig] = count + 1;
+
+	return count + 1;
 }
 
 int
-main(int argc, char* argv[])
+main(int argc, char *argv[])
 {
-	u8 n = 13;
-	int max=0;
+	u8  n   = 13;
+	int max = 0;
 	int num = -1;
 	int i;
 
-	while(n > 1)
-		n = next(n);
+	while (n > 1) n = next(n);
 
-	for(i = 1; i < 1000000; ++i)
+	for (i = 1; i < COUNT; ++i)
 	{
 		int l = len(i);
-		if(l > max){num = i; max = l;}
+		if (l > max)
+		{
+			num = i;
+			max = l;
+		}
 	}
 
 	printf("%d\n", num);
